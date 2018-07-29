9fa4df8ab1e74247d1d9cf99ea44d8bc440346e2
ronen.goren <ronen.goren@gmail.com>
2018-07-29 14:48:40 -0400

    added env

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
+require('dotenv').config()
+
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
-var myAPIKey = process.env.MYAPIKEY;
+var key = process.env.MEETUP_API_KEY
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   request({
-    uri: 'https://api.meetup.com/find/groups?' + key + '&sign=true&photo-host=public&text=Computer+&page=20',
-    qs: {
-      key: '123456',
-    }
+    uri: 'https://api.meetup.com/find/groups?key=' + key + '&sign=true&photo-host=public&text=Computer+Software&page=20',
+    // qs: {
+      // key: '326d1a3e7c615d6f6f2752807a5f7b38',
+      // query: 'World of Warcraft: Legion'
+    // }
   }).pipe(res);
 	// Comment out this line:
   //res.send('respond with a resource');
