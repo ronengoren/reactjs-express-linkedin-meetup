ebc48ba74575ef69de96a17438778f84ea9a9a56
ronen.goren <ronen.goren@gmail.com>
2018-07-29 23:24:52 -0400

    add route

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
